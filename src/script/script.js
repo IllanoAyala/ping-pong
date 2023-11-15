5262d070f8c4de9be69013fad3c0e239ce4ed87c
Illano Ayala <illanoFerreira15@gmail.com>
2023-11-15 11:18:42 -0300

    build(scores): add score basic

--- src/script/script.js
+++ src/script/script.js
@@ -17,15 +17,18 @@ let topPositonPlayer2 = 150;
 let leftPositionPlayer2 = 780;
 let topPositionBall = 200;
 let leftPositionBall = 400;
-let ballDirectionX = -1;
+let ballDirectionX = Math.random() < 0.5 ? 1 : -1;
 let ballDirectionY = 0;
 let step = 7;
 let speedball = 3;
 let start = true;
+let golsP1 = 0;
+let golsP2 = 0;
 
 view.removeChild(player1);
 view.removeChild(player2);
 view.removeChild(ball);
+counterGoals();
 
 const keys = {};
 
@@ -50,20 +53,18 @@ startButton.addEventListener("click", () => {
             if(speedball <= 7)
             {
                 speedball += 1;
-                // transitionColor();
             }
             else{
                 
             }
         }, 3000);
-
     }
     else{
         location.reload();
     }
 });
 
-restartButton.addEventListener("click", () => {
+function restart(inicialDiretionX){
     topPositonPlayer1 = 150;
     leftPositionPlayer1 = 100;
     player1.style.top = 150 + "px";
@@ -77,10 +78,17 @@ restartButton.addEventListener("click", () => {
     topPositionBall = 200;
     leftPositionBall = 400;
     speedball = 3;
-    ballDirectionX = -1;
+    ballDirectionX = inicialDiretionX;
     ballDirectionY = 0;
+    golsP1 = 0;
+    golsP2 = 0;
     ball.style.top = topPositionBall + "px";
     ball.style.left = leftPositionBall + "px";
+    counterGoals();
+}
+
+restartButton.addEventListener("click", () => {
+    restart(ballDirectionX);
 })
 
 document.addEventListener("keydown", (event) => {
@@ -108,16 +116,16 @@ document.addEventListener("keyup", (event) => {
 });
 
 function moverPlayers() {
-    if (player1Keys.up) moverPlayer1(-step);
-    if (player1Keys.down) moverPlayer1(step);
+    if (player1Keys.up) movePlayer1(-step);
+    if (player1Keys.down) movePlayer1(step);
 
-    if (player2Keys.up) moverPlayer2(-step);
-    if (player2Keys.down) moverPlayer2(step);
+    if (player2Keys.up) movePlayer2(-step);
+    if (player2Keys.down) movePlayer2(step);
 
     animationId = requestAnimationFrame(moverPlayers);
 }
 
-function moverPlayer1(topChange) {
+function movePlayer1(topChange) {
     const newTop = topPositonPlayer1 + topChange;
 
     if (newTop >= 0 && newTop + player1Rect.height <= viewRect.height) {
@@ -126,7 +134,7 @@ function moverPlayer1(topChange) {
     }
 }
 
-function moverPlayer2(topChange) {
+function movePlayer2(topChange) {
     const newTop = topPositonPlayer2 + topChange;
 
     if (newTop >= 0 && newTop + player2Rect.height <= viewRect.height) {
@@ -148,13 +156,15 @@ function moverball() {
             ballDirectionX *= -1;
 
             if (leftPositionBall <= 0) {
-                console.log("1");
+                console.log("1"); // function gols
+                counterGoals(player1);
             } else if (leftPositionBall >= viewRect.width - ball.offsetWidth) {
                 console.log("2"); // function gols
+                counterGoals(player2);
             }
         }
 
-        if (topPositionBall <= 1 || topPositionBall >= viewRect.height - ball.offsetHeight) {
+        if (topPositionBall <= 2 || topPositionBall >= viewRect.height - ball.offsetHeight + 2) {
             ballDirectionY *= -1;
         }
 
@@ -183,20 +193,12 @@ function checkCollision() {
             ballDirectionY = -1 * getRandomValueY();
             ballDirectionX *= -1;
         }
-        else if (ballRect.bottom > player1Rect.top) 
-        {
-            ballDirectionY *= 1;
-            ballDirectionX = 0; 
-        } 
-        else if (ballRect.top > player1Rect.bottom) 
-        {
-            ballDirectionY *= 1;
-            ballDirectionX = 0; 
-        }
         else {
             ballDirectionY = 0;
             ballDirectionX *= -1; 
         }
+
+        colorChange();
     }
 
     if (
@@ -213,20 +215,12 @@ function checkCollision() {
             ballDirectionY = -1 * getRandomValueY();
             ballDirectionX *= -1;
         }
-        else if (ballRect.bottom > player2Rect.top) 
-        {
-            ballDirectionY *= 1;
-            ballDirectionX = 0; 
-        } 
-        else if (ballRect.top > player2Rect.bottom) 
-        {
-            ballDirectionY *= 1;
-            ballDirectionX = 0; 
-        }
         else {           
             ballDirectionY = 0;
             ballDirectionX *= -1; 
         }
+
+        colorChange();
     }
 }
 
@@ -234,7 +228,7 @@ function getRandomValueY() {
     return Math.random() * 2;
 }
 
-function randomColor() {
+function getColor() {
     const letters = '0123456789ABCDEF';
     let color = '#';
     for (let i = 0; i < 6; i++) {
@@ -243,11 +237,94 @@ function randomColor() {
     return color;
 }
 
+function randomColor(baseColor) {
+    const variation = 62; // Ajuste conforme necessário
+
+    const offset = () => Math.floor(Math.random() * (2 * variation + 1)) - variation;
+    const clamp = (value) => Math.min(255, Math.max(0, value));
+    const toHex = (value) => (value < 16 ? '0' : '') + value.toString(16);
+
+    const baseRed = parseInt(baseColor.slice(1, 3), 16);
+    const baseGreen = parseInt(baseColor.slice(3, 5), 16);
+    const baseBlue = parseInt(baseColor.slice(5, 7), 16);
+
+    const newRed = clamp(baseRed + offset());
+    const newGreen = clamp(baseGreen + offset());
+    const newBlue = clamp(baseBlue + offset());
+
+    const newColor = `#${toHex(newRed)}${toHex(newGreen)}${toHex(newBlue)}`;
+
+    const luminance = (0.299 * newRed + 0.587 * newGreen + 0.114 * newBlue) / 255;
+    const textColor = luminance > 0.5 ? '#000000' : '#ffffff';
+
+    // Aplica a nova cor de fundo e a cor do texto
+    document.body.style.backgroundColor = newColor;
+    ball.style.backgroundColor = textColor;
+
+    return newColor;
+}
+
 function colorChange() {
-    document.body.style.backgroundColor = randomColor();
-    view.style.backgroundColor = randomColor();
-    player1.style.backgroundColor = randomColor();
+    const baseColor = getColor(); // Cor base, substitua pela cor desejada
+
+    document.body.style.backgroundColor = randomColor(baseColor);
+    view.style.backgroundColor = randomColor(baseColor);
+    player1.style.backgroundColor = randomColor(baseColor);
     player2.style.backgroundColor = player1.style.backgroundColor;
 }
 
-setInterval(colorChange, 5000);
+function counterGoals(player){
+    var scores = document.querySelectorAll("#score");
+
+    if(player === player1)
+    {
+        golsP1 += 1;
+        scores[1].textContent = golsP1;
+        topPositonPlayer1 = 150;
+        leftPositionPlayer1 = 100;
+        player1.style.top = 150 + "px";
+        player1.style.left = 100 + "px";
+        
+        topPositonPlayer2 = 150;
+        leftPositionPlayer2 = 780;
+        player2.style.top = 150 + "px";
+        player2.style.left = 780 + "px";
+
+        topPositionBall = 200;
+        leftPositionBall = 400;
+        speedball = 3;
+        ballDirectionX = -1;
+        ballDirectionY = 0;
+        ball.style.top = topPositionBall + "px";
+        ball.style.left = leftPositionBall + "px";
+    }
+    else if(player === player2){
+        golsP2 += 1;
+        scores[0].textContent = golsP2;
+        topPositonPlayer1 = 150;
+        leftPositionPlayer1 = 100;
+        player1.style.top = 150 + "px";
+        player1.style.left = 100 + "px";
+        
+        topPositonPlayer2 = 150;
+        leftPositionPlayer2 = 780;
+        player2.style.top = 150 + "px";
+        player2.style.left = 780 + "px";
+
+        topPositionBall = 200;
+        leftPositionBall = 400;
+        speedball = 3;
+        ballDirectionX = 1;
+        ballDirectionY = 0;
+        ball.style.top = topPositionBall + "px";
+        ball.style.left = leftPositionBall + "px";    
+    }
+    else{
+        scores[0].textContent = golsP2;
+        scores[1].textContent = golsP1;
+    }
+
+    
+}
+// setInterval(colorChange, 5000);
+// clearInterval(colorChange);
